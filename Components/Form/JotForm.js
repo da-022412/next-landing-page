b733b2362b5f7d124e4689bffc5aa6ae96212e1c
Dennis Acosta <email@dennisacosta.me>
2021-06-25 08:26:15 -0400

    Add untracked files

---
+++ Components/Form/JotForm.js
@@ -0,0 +1,14 @@
+import { Box } from '@material-ui/core';
+
+const Form = () => {
+    return (
+        <Box id='form'>
+            <script
+                type='text/javascript'
+                src='https://form.jotform.com/jsform/211724001033132'
+            ></script>
+        </Box>
+    );
+};
+
+export default Form;
