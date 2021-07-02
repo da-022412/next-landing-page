e51b42b402a2aa9fbb6efa4b1425b89742048c24
Dennis Acosta <email@dennisacosta.me>
2021-07-02 09:04:21 -0400

    Update

--- out/_next/static/V8kD7Fu9mX58pZYNh7D4d/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2e8ffb5c9eeb90afd767.js"],"/_error":["static/chunks/pages/_error-a9f53acb468cbab8a6cb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
